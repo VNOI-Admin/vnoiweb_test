045a7c3ba2cd8fe29424b95f62db4f81097195bf
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-04 00:10:39 +0800

    Automatically take screenshot when test fail

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ gem 'rspec', '~>3.0.0'
 gem 'selenium-webdriver', '~> 2.44.0'
 gem 'json', '~> 1.8.1'
 gem 'pry', '~> 0.10'
+gem 'capybara-screenshot', '~> 0.3.21'
+
--- helper.rb
+++ helper.rb
@@ -1,6 +1,8 @@
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/rspec'
+require 'capybara-screenshot'
+require 'capybara-screenshot/rspec'
 
 RSpec.configure do |config|
   config.include Capybara::DSL, type: :feature
@@ -13,6 +15,10 @@ Capybara.default_driver = :selenium
 Capybara.default_selector = :css
 # Set default wait time for page elements lookup to 5 seconds
 Capybara.default_wait_time = 5
+# HTML files & screenshots at failure points will be saved in output folder
+Capybara.save_and_open_page_path = './output'
+Capybara::Screenshot::RSpec.add_link_to_screenshot_for_failed_examples = true
+Capybara::Screenshot::RSpec::REPORTERS['RSpec::Core::Formatters::HtmlFormatter'] = Capybara::Screenshot::RSpec::HtmlEmbedReporter
 
 ROOT_URL = 'http://localhost:8000'
 
@@ -21,4 +27,8 @@ def verify_content(texts)
     puts "Looking for string '#{text}'"
     expect(page).to have_content(text)
   }
+end
+
+def random_string(len)
+  (0...length).map { (65 + rand(26)).chr }.join
 end
