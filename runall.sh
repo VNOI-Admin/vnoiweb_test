ead4d1ef7a751854471a85d80b43f78970c195b8
Trung, Nguyen Thanh <thanhtnguyen@paypal.com>
2015-03-04 00:26:36 +0800

    Add README.md. Concat report files to single HTML file.

--- runall.sh
+++ runall.sh
@@ -2,3 +2,6 @@ for f in spec/*.rb; do
   echo "Executing test: $f"
   bundle exec rspec $f --format documentation --format html --out "output/$f.html"
 done
+
+rm -f report.html
+cat output/spec/*.html > report.html
